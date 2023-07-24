29264776dbc949a8440819005a833ae621a62bb0
Volodymyr Baginskyi <vova27n@gmail.com>
2023-07-24 22:16:55 +0300

    add some in Java files

--- dz-8/src/main/java/org/example/Student.java
+++ dz-8/src/main/java/org/example/Student.java
@@ -1,2 +1,21 @@
-package org.example;public class Student {
+package org.example;
+
+public class Student {
+    private int student_id;
+    private String first_name;
+    private String last_name;
+
+    public Student(int student_id, String first_name, String last_name) {
+        this.student_id = student_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+    public int getStudentId() {
+        return student_id;
+    }
+
+    public String getFullName() {
+        return first_name + " " + last_name;
+    }
 }
--- dz-8/src/main/java/org/example/StudentsGroup.java
+++ dz-8/src/main/java/org/example/StudentsGroup.java
@@ -1,2 +1,49 @@
-package org.example;public class StudentsGroup {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentsGroup {
+    private Student captain;
+    private List<Student> students;
+    private List<String> tasks;
+
+    public StudentsGroup(Student captain) {
+        this.captain = captain;
+        this.students = new ArrayList<>();
+        this.tasks = new ArrayList<>();
+    }
+
+    public void changeCaptain(Student newCaptain) {
+        this.captain = newCaptain;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void removeStudent(Student student) {
+        students.remove(student);
+    }
+
+    public void addTask(String task) {
+        tasks.add(task);
+    }
+
+    public boolean markTaskDone(Student student, String task) {
+        int studentId = student.getStudentId();
+        for (Student s : students) {
+            if (s.getStudentId() == studentId) {
+                // Assume each task is unique, otherwise, you can use a task_id instead of a task string
+                if (tasks.contains(task)) {
+                    // Here, you can perform any additional checks or validation before marking the task done.
+                    tasks.remove(task);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
 }
